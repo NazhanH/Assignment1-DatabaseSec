9023a7c8c1243fe9706313ad7d05d68ee9e6dabe
nazhanh <1221301122@student.mmu.edu.my>
2024-05-07 00:14:47 +0800

    added user roles, i think, maybe

--- src/book/views.py
+++ src/book/views.py
@@ -3,21 +3,42 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 
+from django.contrib.auth.models import Group
+
+from django.contrib import messages
+
+from .forms import CreateUserForm
+
+from .decoraters import unauthenticated_user
+
 # Create your views here.
 def home(request, *args, **kwargs):
     return render(request, 'home.html')
 
+@unauthenticated_user
 def register(request):
+
+    form = CreateUserForm()
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
         if form.is_valid():
             user = form.save()
+            username = form.cleaned_data.get('username')
+
+            group = Group.objects.get(name='Patrons')
+            user.groups.add(group)
+
+            messages.success(request, 'Account was created for ' + username)
+            
+            
+
             auth_login(request, user)
             return redirect('home')
-    else:
-        form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+        
+    context = {'form': form}
+    return render(request, 'register.html', context)
 
+@unauthenticated_user
 def login(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
